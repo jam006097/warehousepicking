992b141bc436cf14484a86b9b58cbae51932ca9d
jam <jam006097@gmail.com>
2024-10-27 15:39:24 +0900

    追加、削除、編集機能の追加

--- Services/ProductListApp.cs
+++ Services/ProductListApp.cs
@@ -8,15 +8,48 @@ namespace PickingRoute.Services
 	{
 		private readonly ApplicationDbContext _context;
 
+		// コンストラクタ
 		public ProductListService(ApplicationDbContext context)
 		{
 			_context = context;
 		}
 
-		public async Task<List<ProductItem>> GetProductAsync()
+		// 商品一覧を取得
+		public async Task<List<ProductItem>> GetProductsAsync()
 		{
 			var items = await _context.ProductItems.ToListAsync();
 			return items;
+		}
+
+		// 商品の更新
+		public async Task UpdateProductAsync(ProductItem productItemInput)
+		{
+			var existingProductItem = await _context.ProductItems.FindAsync(productItemInput.ProductId);
+			if (existingProductItem != null)
+			{
+				existingProductItem.ProductName = productItemInput.ProductName;
+				existingProductItem.strangeLocationX = productItemInput.strangeLocationX;
+				existingProductItem.strangeLocationY = productItemInput.strangeLocationY;
+				await _context.SaveChangesAsync();
+			}
+		}
+
+		// 商品の削除
+		public async Task DeleteProductAsync(int id)
+		{
+			var existingProductItem = await _context.ProductItems.FindAsync(id);
+			if (existingProductItem != null)
+			{
+				_context.ProductItems.Remove(existingProductItem);
+				await _context.SaveChangesAsync();
+			}
+		}
+
+		// 商品の追加
+		public async Task AddProductAsync(ProductItem newProductItem)
+		{
+			_context.ProductItems.Add(newProductItem);
+			await _context.SaveChangesAsync();
 		}
 	}
 }
