c868e6cd9e5bd90497bd4ce16821bc001ba561b0
jam <jam006097@gmail.com>
2024-10-29 06:43:26 +0900

    編集コンポーネントの作成

--- Models/ProductItem.cs
+++ Models/ProductItem.cs
@@ -7,13 +7,14 @@ namespace PickingRoute.Models
 		[Key]
 		public int ProductId { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "ProductName is required.")]
+		[StringLength(10, ErrorMessage = "ProductName must be less than 10 characters.")]
 		public string ProductName { get; set; } = string.Empty;
 
-		[Required]
+		[Required(ErrorMessage = "StrangeLocationX is required.")]
 		public int strangeLocationX { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "StrangeLocationY is required.")]
 		public int strangeLocationY { get; set; }
 	}
 }
