d05a1c177f160f4592be09c350698e578d23888e
jam <jam006097@gmail.com>
2024-12-16 06:28:41 +0900

    デザインをモダンな感じに修正

---
+++ wwwroot/js/EditWarehouse.js
@@ -0,0 +1,22 @@
+﻿<script>
+function UpdateInputWidth() {
+    var inputs = document.querySelectorAll('input[type="text"]');
+    inputs.forEach(function(input) {
+        input.style.width = (input.value.length + 1) + 'ch';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    UpdateInputWidth();
+});
+
+window.domInterop = {
+    updateInputWidth: function (shelfId) {
+        var inputElement = document.getElementById('shelf-' + shelfId);
+        if (inputElement) {
+            inputElement.style.width = (inputElement.value.length + 1) + 'ch';
+        }
+    }
+};
+
+</script>
