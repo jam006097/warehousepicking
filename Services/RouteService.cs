a64195f2b6d293660ee8d180a47deb1a6247c14a
jam <jam006097@gmail.com>
2024-12-21 22:08:25 +0900

    ルート作成ページ作成完了

--- Services/RouteService.cs
+++ Services/RouteService.cs
@@ -6,5 +6,7 @@ namespace PickingRoute.Services
 	public class RouteService
 	{
 		public List<RoutePoint> Routes { get; set; } = new List<RoutePoint>();
+		public List<ProductItem> optimalItems { get; set; } = new List<ProductItem>();
+		public List<ProductItem> PickingItems { get; set; } = new List<ProductItem>(); 
 	}
 }
